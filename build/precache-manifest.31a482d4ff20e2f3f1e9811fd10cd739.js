8514504eb305bbbd7b803ce0134f9d8fbe2c63ee
Diego Paiva <paivadiego7797@gmail.com>
2022-08-03 12:10:11 +0100

    Build with tailwindcss

---
+++ build/precache-manifest.31a482d4ff20e2f3f1e9811fd10cd739.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cce132efc65509bd8341",
+    "url": "/static/css/main.9f9e1a00.chunk.css"
+  },
+  {
+    "revision": "cce132efc65509bd8341",
+    "url": "/static/js/main.cce132ef.chunk.js"
+  },
+  {
+    "revision": "faaf40f48ceb94201a63",
+    "url": "/static/js/1.faaf40f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdfe47b31e9184a55cf02eef1baf7240",
+    "url": "/static/media/Azonix.cdfe47b3.otf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat.ee653992.ttf"
+  },
+  {
+    "revision": "94ad777514f2b4f33e0870ca231bfcfb",
+    "url": "/index.html"
+  }
+];
